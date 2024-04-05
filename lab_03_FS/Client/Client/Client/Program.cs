330906b16d20ab541ef1e9a25d7b78151ae4bbba
Constantine <two-slashes@yandex.ru>
2024-04-05 09:52:11 +0300

    Lab_03 part 1

---
+++ lab_03_FS/Client/Client/Client/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Metadata;
+using System.Text;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        string serverIP = "127.0.0.1"; 
+        int serverPort = 8888;
+
+        while (true) 
+        {
+            try
+            {
+                using (Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+                {
+                    clientSocket.Connect(IPAddress.Parse(serverIP), serverPort);
+                    NetworkStream stream = new NetworkStream(clientSocket);
+
+
+                    string action;
+
+                    string fileName;
+                    string request;
+                    string response;
+
+                    Console.WriteLine("Enter action: 1 - get a file, 2 - create file, 3 - delete a file");
+                    action = Console.ReadLine();
+
+                    switch (action)
+                    {
+                        case "1":
+                            Console.WriteLine("Enter file name:");
+                            fileName = Console.ReadLine();
+                            request = $"GET {fileName}";
+
+                            SendRequest(stream, request);
+                            response = ReceiveResponse(stream);
+
+                            if (response.StartsWith("200"))
+                            {
+                                Console.WriteLine("File content: " + response.Substring(4));
+                            }
+
+                            else if (response.StartsWith("404"))
+                            {
+                                Console.WriteLine("The response says that the file was not found!");
+                            }
+
+                            break;
+
+                        case "2":
+                            Console.WriteLine("Enter file name:");
+                            fileName = Console.ReadLine();
+
+                            Console.WriteLine("Enter file content:");
+                            string fileContent = Console.ReadLine();
+
+
+                            request = $"PUT {fileName} {fileContent}";
+                            SendRequest(stream, request);
+                            response = ReceiveResponse(stream);
+
+                            if (response.StartsWith("200"))
+                            {
+                                Console.WriteLine("The response says that file was created!");
+                            }
+
+                            else if (response.StartsWith("403"))
+                            {
+                                Console.WriteLine("The response says that creating the file was exists!");
+                            }
+
+                            break;
+
+                        case "3":
+                            Console.WriteLine("Enter file name:");
+                            fileName = Console.ReadLine();
+
+                            request = $"DELETE {fileName}";
+                            SendRequest(stream, request);
+                            response = ReceiveResponse(stream);
+
+                            if (response.StartsWith("200"))
+                            {
+                                Console.WriteLine("The response says that the file was successfully deleted!.");
+                            }
+
+                            else if (response.StartsWith("404"))
+                            {
+                                Console.WriteLine("The response says that the file was not found!");
+                            }
+
+                            break;
+
+
+                        case "exit":
+                            request = "exit";
+                            SendRequest(stream, request);
+
+                            break;
+                    }
+
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+
+    static void SendRequest(NetworkStream stream, string request)
+    {
+        byte[] requestData = Encoding.UTF8.GetBytes(request);
+        stream.Write(requestData, 0, requestData.Length);
+    }
+
+    static string ReceiveResponse(NetworkStream stream)
+    {
+        byte[] responseData = new byte[256];
+        int bytesRead = stream.Read(responseData, 0, responseData.Length);
+        return Encoding.UTF8.GetString(responseData, 0, bytesRead);
+    }
+}
---
+++ lab_03_FS/Server/Server/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+class Server
+{
+    static void Main(string[] args)
+    {
+        int port = 8888;
+        string dataDir = $"./server/data/"; 
+
+        Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, port);
+ 
+        listener.Bind(localEndPoint);
+        listener.Listen(10);
+
+        Console.WriteLine("Server started!");
+
+        while (true)
+        {
+            Socket handler = listener.Accept();
+            byte[] data = new byte[256];
+            int bytes = handler.Receive(data);
+            string request = Encoding.UTF8.GetString(data, 0, bytes);
+            string[] requestData = request.Split(' ');
+
+            string response;
+
+            if (requestData[0].Equals("exit"))
+            {
+                response = "Server stopped!";
+                byte[] exitResponse = Encoding.UTF8.GetBytes(response);
+                handler.Send(exitResponse);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+                break;
+            }
+
+            else if (requestData[0].Equals("PUT"))
+            {
+                string fileName = requestData[1];
+                string fileContent = requestData[2];
+
+                string filePath = $"./server/data/{fileName}";
+                if (File.Exists(filePath))
+                {
+                    File.WriteAllText(filePath, fileContent);
+                    response = "403"; // Файл создан 
+                }
+
+                else
+                {
+                    File.WriteAllText(filePath, fileContent);
+                    response = "200"; // Файл создан 
+                }
+            }
+
+            else if (requestData[0].Equals("GET"))
+            {
+                string fileName = requestData[1];
+                string filePath = Path.Combine(dataDir, fileName);
+
+                if (File.Exists(filePath))
+                {
+                    string fileContent = File.ReadAllText(filePath);
+                    response = "200 " + fileContent; // Файл существует
+                }
+
+                else
+                {
+                    response = "404"; // Файл не найден
+                }
+            }
+
+            else if (requestData[0].Equals("DELETE"))
+            {
+                string fileName = requestData[1];
+                string filePath = Path.Combine(dataDir, fileName);
+
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                    response = "200"; // Файл удален
+                }
+
+                else
+                {
+                    response = "404"; // Файл не найден
+                }
+            }
+
+            else
+            {
+                response = "400"; // Неправильный запрос
+            }
+
+            byte[] responseData = Encoding.UTF8.GetBytes(response);
+            handler.Send(responseData);
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+    }
+}
